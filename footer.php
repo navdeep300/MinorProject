eeae4519d9d37ad7de5b760fb814e78d347c602c
Navdeep Singh <navdeepsaini48@gmail.com>
2015-11-06 20:00:26 +0530

    footer file

---
+++ footer.php
@@ -0,0 +1,12 @@
+<ul style='list-style-type: none;'>
+	<li>
+		<a href='home.php'>Home</a>
+	</li>
+	<li>
+		<a href='about.php'>About</a>
+	</li>
+	<li>
+		<a href='contactus.php'>Feedback</a>
+	</li>
+</ul>
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
