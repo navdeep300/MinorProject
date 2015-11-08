a9a27748fd8215d7aa52d50a1085f49de26579ad
Navdeep Singh <navdeepsaini48@gmail.com>
2015-11-08 20:27:06 +0530

    index file

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<?php
+
+header('location:main.php');
+
+require 'navigation.php';
+?>
+<?php
+require 'footer.php';
+// if ($_SESSION['id']=="") {
+// 	//when not logined
+// 	$
+// }
+
+// switch(@$_REQUEST["id"])
+// {
+//  case 'home':
+//  include("home.php");
+//  break;
+ 
+//  case 'login':
+//  include("login.php");
+//  break;
+ 
+//  case 'logout':
+//  include("logout.php");
+//  break;
+ 
+//  case 'feedback':
+//  include("feedback.php");
+//  break;
+
+//  case 'about':
+//  	include 'about.php';
+//  	break;
+
+ // default:
+ // include("home.php");
+ // break;
+// }
+?>
